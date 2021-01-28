7dee0a598d0f5a398649930254ff96a0e4a33baf
lihao <lihao@blockc.pro>
2021-01-28 17:46:21 +0800

    新增日志处理模块

---
+++ app/core/logger.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import logging
+
+from setting import load_config
+
+
+cfg = load_config()['log']
+
+loggers = {}
+
+def get_logger(filename):
+    global loggers
+
+    if filename in loggers:
+        logger = loggers[filename]
+    else:
+        if not os.path.exists(cfg['path']):
+            os.mkdir(cfg['path'])
+        filepath = os.path.join(cfg['path'], filename + '.log')
+        logger = logging.getLogger()
+        logger.setLevel(cfg['level'])
+        
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+        if cfg['stdout']:
+            stream_handler = logging.StreamHandler(sys.stdout)
+            stream_handler.setLevel(cfg['level'])
+            stream_handler.setFormatter(formatter)
+            logger.addHandler(stream_handler)
+
+        file_handler = logging.handlers.TimedRotatingFileHandler(
+            filepath, when="d", interval=1, backupCount=10, encoding='utf-8')
+        file_handler.setLevel(cfg['level'])
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        loggers[filename] = logger
+    return logger
