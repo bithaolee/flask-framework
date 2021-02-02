25c8be1adb9078bfa328faedfbf8c921b5706471
lihao <lihao@blockc.pro>
2021-02-02 14:35:46 +0800

    update

--- app/core/db.py
+++ app/core/db.py
@@ -8,18 +8,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from .config import config
 
 
-db_cfg = config('db')
+_db_cfg = config('db')
 
 engine = create_engine(
-    f'mysql+pymysql://{db_cfg["username"]}:{db_cfg["password"]}@{db_cfg["host"]}:{db_cfg["port"]}/{db_cfg["database"]}',
-    echo=db_cfg['echo'],
+    f'mysql+pymysql://{_db_cfg["username"]}:{_db_cfg["password"]}@{_db_cfg["host"]}:{_db_cfg["port"]}/{_db_cfg["database"]}',
+    echo=_db_cfg['echo'],
     max_overflow=0,  # 超过连接池大小外最多创建的连接
     pool_size=5,  # 连接池大小
     pool_timeout=30,  # 池中没有线程最多等待的时间，否则报错
     pool_recycle=-1  # 多久之后对线程池中的线程进行一次连接的回收(重置)
 )
 
-db_session = scoped_session(
+_db_session = scoped_session(
     sessionmaker(
         autocommit=False,
         autoflush=False,
@@ -27,14 +27,14 @@ db_session = scoped_session(
     )
 )
 
-def get_db():
+def _get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = db_session()
+        db = g._database = _db_session()
     return db
 
 
-db = LocalProxy(get_db)
+db = LocalProxy(_get_db)
 
 
 def transaction(fn):
